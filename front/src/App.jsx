256b6cfd067b58ff3968b60111e1b583050ef31a
Tetiana Tkachuk <tanyatkachuk2401@gmail.com>
2021-07-29 18:25:51 +0200

    Added gallery section

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,6 +5,8 @@ import MenuPage from './layouts/MenuPage/MenuPage'
 import BookPage from './layouts/BookPage/BookPage'
 import GalleryPage from './layouts/GalleryPage/GalleryPage'
 
+const neonColors = ['#FFFF00', '#E6FB04', '#FF0000', '#FF6600', '#00FF00', '#00FF66', '#00FFFF', '#099FFF', '#0033FF', '#FF00FF', '#FF0099', '#d400ff']
+
 function App() {
 	return (
 		<div>
--- front/src/layouts/WelcomePage/Logo/Logo.jsx
+++ front/src/layouts/WelcomePage/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import styles from './Logo.module.scss'
-import logo from '../../../logo.svg'
+import logo from '../../../assets/logo.svg'
 
 const Logo = () => {
 	return <img className={styles.logo} src={logo} alt="Hell's Kitchen Logo" />

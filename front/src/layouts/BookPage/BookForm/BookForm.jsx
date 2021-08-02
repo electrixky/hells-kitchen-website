aefc67ea282dbbf2aea86a4d21f57b90db108b30
Tetiana Tkachuk <tanyatkachuk2401@gmail.com>
2021-08-02 18:01:05 +0200

    Changed booking Inputs to buttons

--- front/src/layouts/BookPage/BookForm/BookForm.jsx
+++ front/src/layouts/BookPage/BookForm/BookForm.jsx
@@ -1,37 +1,37 @@
 import styles from './BookForm.module.scss'
 import Heading from './Heading/Heading'
-import InputDate from './InputDate/InputDate'
-import Input from './Input/Input'
+import ButtonDate from './ButtonDate/ButtonDate'
+import Button from './Button/Button'
 import Submission from './Submission/Submission'
 
 const BookForm = () => {
 	return <div className={styles.form}>
 		<Heading>Date</Heading>
 		<div className={styles.inputs}>
-			<InputDate day='Mo' date='16' />
-			<InputDate day='Tu' date='17' />
-			<InputDate day='We' date='18' />
-			<InputDate day='Th' date='19' />
-			<InputDate day='Fr' date='20' />
-			<InputDate day='Sa' date='21' />
-			<InputDate day='Su' date='22' />
+			<ButtonDate day='Mo' date='16' />
+			<ButtonDate day='Tu' date='17' />
+			<ButtonDate day='We' date='18' />
+			<ButtonDate day='Th' date='19' />
+			<ButtonDate day='Fr' date='20' />
+			<ButtonDate day='Sa' date='21' />
+			<ButtonDate day='Su' date='22' />
 		</div>
 
 		<Heading>Time</Heading>
 		<div className={styles.inputs}>
-			<Input value='18pm'/>
-			<Input value='19pm'/>
-			<Input value='20pm'/>
-			<Input value='21pm'/>
-			<Input value='22pm'/>
+			<Button value='18pm'/>
+			<Button value='19pm'/>
+			<Button value='20pm'/>
+			<Button value='21pm'/>
+			<Button value='22pm'/>
 		</div>
 
 		<Heading>People</Heading>
 		<div className={styles.inputsPeople}>
-			<Input value='1-2'/>
-			<Input value='3-4'/>
-			<Input value='5-6'/>
-			<Input value='>6'/>
+			<Button value='1-2'/>
+			<Button value='3-4'/>
+			<Button value='5-6'/>
+			<Button value='>6'/>
 		</div>
 
 		<Submission/>
